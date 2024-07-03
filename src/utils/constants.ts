facd9c2070dcf8f0d85d4012ac21587929caa4fb
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-02 21:39:28 -0300

    feat: constants for Navbar rendering

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,17 +1,11 @@
 interface PagesNavbarHeader {
-  [key: string]: {
-    name: string
-    link: string
-  }
+  name: string
+  link: string
 }
 
-const PagesNavbarData = (route: string) => {
-  const data: PagesNavbarHeader = {
-    home: { name: 'Home', link: '/' },
-    projects: { name: 'Projects', link: '/projects' },
-    aboutme: { name: 'About me', link: '/aboutme' },
-    contact: { name: 'Contact', link: '/contact' }
-  }
-
-  return data[route]
-}
+export const pagesNavbarData: PagesNavbarHeader[] = [
+  { name: 'Home', link: '/' },
+  { name: 'Projects', link: '/projects' },
+  { name: 'About me', link: '/aboutme' },
+  { name: 'Contact', link: '/contact' }
+]
