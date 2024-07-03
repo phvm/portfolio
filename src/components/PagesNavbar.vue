4d370c3f2ada89659e079800655814f642f1f918
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-02 21:48:31 -0300

    feat: starting navbar component

--- src/components/PagesNavbar.vue
+++ src/components/PagesNavbar.vue
@@ -1,7 +1,11 @@
 <template>
-  <nav></nav>
+  <nav>
+    <div v-for="data in pagesNavbarData" :key="data.name">{{ data.name }}</div>
+  </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { pagesNavbarData } from '../utils/constants'
+</script>
 
 <style scoped></style>
