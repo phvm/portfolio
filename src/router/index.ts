4d92462e2b007328ce81773097349632b4d6a722
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-05 22:21:31 -0300

    feat: removed aboutme page and route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import ProjectsView from '@/views/ProjectsView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import ProjectsView from '@/views/ProjectsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { name: 'home', path: '/', component: HomeView },
     { name: 'projects', path: '/projects', component: ProjectsView },
-    { name: 'aboutme', path: '/aboutme', component: HomeView },
     { name: 'contact', path: '/contact', component: HomeView }
   ]
-})
+});
 
-export default router
+export default router;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,7 @@
-import type { PagesNavbarHeader } from '@/types/componentTypes'
+import type { PagesNavbarHeader } from '@/types/componentTypes';
 
 export const pagesNavbarData: PagesNavbarHeader[] = [
   { name: 'Home', link: '/' },
   { name: 'Projects', link: '/projects' },
-  { name: 'About me', link: '/aboutme' },
   { name: 'Contact', link: '/contact' }
-]
+];
