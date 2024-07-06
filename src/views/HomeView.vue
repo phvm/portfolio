15de0721e5284180a5683c669a20ecec28e6a813
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-05 22:21:46 -0300

    feat: initial home page and bio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <article class="flex flex-col justify-center px-7 py-10 m-auto w-5/6">
+    <h1 class="text-4xl mb-4">Pedro Moura</h1>
+    <p class="text-lg mb-4">
+      Hi, I'm Pedro Moura, a software developer specialized in building <b>robust</b> and <b>blazing fast</b> web
+      applications.
+    </p>
+    <p class="text-base">
+      I've been building software for over 4 years, developing web apps that impacted thousands of users for different
+      companies.
+    </p>
+  </article>
 </template>
 
 <script setup lang="ts"></script>
