903f455ac77625436f292385fd4a25f7e85cce80
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-05 22:10:48 -0300

    feat: navbar font size

--- src/components/PagesNavbar.vue
+++ src/components/PagesNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between pr-3 py-4 border-b-2 sticky">
+  <header class="flex justify-between pr-3 py-4 border-b-2 sticky text-base">
     <h1 class="ml-2">Pedro Moura</h1>
     <nav>
       <NavbarButton v-for="data in pagesNavbarData" :key="data.name" :link="data.link" :name="data.name" />
