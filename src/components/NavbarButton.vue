fa8ee0b966350653d6aafe50faa97895249a2a9c
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-05 21:15:26 -0300

    feat: navbar buttons component

---
+++ src/components/NavbarButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterLink :to="props.link" class="px-1 mx-1">
+    <button type="button">{{ props.name }}</button>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import type { PagesNavbarHeader } from '../types/componentTypes';
+import { RouterLink } from 'vue-router';
+
+const props = defineProps<PagesNavbarHeader>();
+</script>
+
+<style scoped></style>
