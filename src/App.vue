96a41db144a627b618c60e65b012ca696552e6e2
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-05 22:27:55 -0300

    feat: placeholder for logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import PagesFooter from './components/PagesFooter.vue'
-import PagesNavbar from './components/PagesNavbar.vue'
+import { RouterView } from 'vue-router';
+import PagesFooter from './components/PagesFooter.vue';
+import PagesNavbar from './components/PagesNavbar.vue';
 </script>
 
 <template>
   <PagesNavbar />
-  <main><RouterView /></main>
-  <footer>
-    <PagesFooter />
-  </footer>
+  <main>
+    <RouterView />
+  </main>
 </template>
--- src/components/PagesNavbar.vue
+++ src/components/PagesNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex justify-between pr-3 py-4 border-b-2 sticky text-base">
-    <h1 class="ml-2">Pedro Moura</h1>
+    <h1 class="ml-2">Placeholder logo</h1>
     <nav>
       <NavbarButton v-for="data in pagesNavbarData" :key="data.name" :link="data.link" :name="data.name" />
     </nav>
