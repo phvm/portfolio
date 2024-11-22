ffc568243484910678ab7040c71738e65da796c2
phvm <pedrohvdm@gmail.com>
2024-11-22 02:24:55 -0300

    feat: home texts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
   <main class="w-screen h-screen">
     <h1 class="text-center">Hi, my name is Pedro Moura</h1>
     <h2 class="text-center">I'm a software developer, specialized in front-end development.</h2>
+  <main class="bg-background w-screen h-screen flex flex-col justify-center items-center pb-16">
+    <h1 class="text-center text-4xl text-faded">Hi, I'm <span class="font-bold">Pedro Moura</span></h1>
+    <h2 class="text-center text-xl text-foreground">I'm a software developer, specialized in front-end development.</h2>
   </main>
 </template>
 
