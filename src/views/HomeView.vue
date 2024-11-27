52501df9936d4673426d829a7324f02ad44b5a7a
phvm <pedrohvdm@gmail.com>
2024-11-27 19:56:15 -0300

    fix: page hierarchy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="w-screen h-screen flex flex-col justify-center items-center pb-16">
-    <h1 class="text-center text-3xl text-faded md:text-3xl lg:text-5xl">
-      Hi, I'm <strong class="font-bold">Pedro Moura</strong>
-    </h1>
-    <h2 class="text-center text-base px-2 md:w-1/2 md:text-xl lg:w-1/4">
-      I'm a software developer, specialized in <strong>front-end development.</strong>
+    <h1 class="text-center text-base md:text-xl">Hi, I'm Pedro Moura</h1>
+    <h2 class="text-center my-2 text-faded text-3xl px-2 md:w-2/3 lg:text-4xl">
+      I'm a software developer, specialized in <strong class="text-accent">front-end development.</strong>
     </h2>
+    <h3 class="text-center md:w-1/2">
+      My favorite work is to build software that looks great, is accessible to everyone and is blazing fast,
+      meticulously crafting with accessibility, responsiveness and performance in mind.
+    </h3>
     <nav class="flex flex-col items-center w-full mt-4 md:flex-row md:justify-evenly md:w-2/3 lg:w-1/2">
       <NavigationLink
         v-for="(navLink, index) in navLinks"
