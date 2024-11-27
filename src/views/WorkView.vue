44fa0ff8e5cc2c7f69f904240706e257bc0aea45
phvm <pedrohvdm@gmail.com>
2024-11-27 19:56:43 -0300

    fix: experiences responsiveness

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="flex flex-col h-screen md:flex-row">
-    <aside class="w-1/2 flex flex-col justify-center items-center">
+  <div class="flex flex-col pt-4 justify-center md:justify-end md:flex-row lg:pr-52">
+    <aside class="flex flex-col justify-center items-center p-2 md:w-1/4 lg:fixed lg:left-20">
       <h2>Pedro Moura</h2>
       <h3>Front-end Developer</h3>
-      <p>I've worked with some different teams building responsive, accessible interfaces.</p>
+      <p class="text-center">I've worked with some different teams building responsive, accessible interfaces.</p>
     </aside>
-    <main class="w-1/2">
+    <main class="flex flex-col justify-center md:p-10 md:w-3/4 lg:w-2/5">
       <ExperienceCard
         v-for="(experience, index) in experienceList"
         :key="index"
+        :company="experience.company"
         :description="experience.description"
         :end-date="experience.endDate"
         :experience-title="experience.experienceTitle"
