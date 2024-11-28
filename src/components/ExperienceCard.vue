6967f70c646ef220c09036c3bc8f0516791872d0
Pedro Moura <Pedrohvdm@gmail.com>
2024-11-28 08:22:55 -0300

    feat: removed experience card paddings

--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -20,11 +20,11 @@ const props = defineProps<Props>();
     <span class="w-1/3 min-w-40 text-center md:text-right group-hover:text-accent group-hover:font-bold"
       >{{ props.startingDate }} - {{ props.endDate }}</span
     >
-    <div class="px-8 md:pl-4 md:pr-6 md:w-2/3">
+    <div class="px-8 md:w-2/3">
       <span class="text-center w-full inline-block md:text-left group-hover:text-accent group-hover:font-bold">{{
         `${experienceTitle} - ${company}`
       }}</span>
-      <p class="my-2">{{ description }}</p>
+      <p class="my-4">{{ description }}</p>
       <div class="flex flex-wrap justify-center md:justify-normal">
         <SkillChip v-for="(skill, index) in skills" :key="index" :skill-name="skill" />
       </div>
