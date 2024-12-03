830461fb9c90a223af102b54847839f0695a98c5
Pedro Moura <Pedrohvdm@gmail.com>
2024-12-03 11:51:40 -0300

    feat: re-styled home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,43 @@
 <template>
-  <main class="w-screen h-screen flex flex-col justify-center items-center pb-16">
-    <ThemeSelector />
-    <h1 class="text-center text-base md:text-xl">Hi, I'm Pedro Moura</h1>
-    <h2 class="text-center my-2 text-faded text-3xl px-2 md:w-2/3 lg:text-4xl">
-      I'm a software developer, specialized in <strong class="text-accent">front-end development.</strong>
+  <main class="flex flex-col px-2">
+    <h1 class="text-xl font-bold md:text-3xl text-left w-full">Pedro Moura</h1>
+    <h2 class="my-4 text-lg md:w-2/3">
+      Hey, I'm Pedro Moura, I'm a software developer specialized in
+      <strong class="text-accent">front-end development.</strong>
     </h2>
-    <h3 class="text-center md:w-1/2">
+    <p>
       My favorite work is to build software that looks great, is accessible to everyone and is blazing fast,
       meticulously crafting with accessibility, responsiveness and performance in mind.
-    </h3>
-    <nav class="flex flex-col items-center w-full mt-4 md:flex-row md:justify-evenly md:w-2/3 lg:w-1/2">
-      <NavigationLink
-        v-for="(navLink, index) in navLinks"
-        :key="index"
-        :link="navLink.linkTo"
-        :text="navLink.pageTitle"
-      />
-    </nav>
+    </p>
+    <p>
+      You can see some
+      <NavigationLink :link="navLinks.projects.link">projects I've already built</NavigationLink>
+      (sometimes with wonderful people) or if you're curious about my past as a software developer,
+      <NavigationLink :link="navLinks.experiences.link"> you can go here.</NavigationLink>
+    </p>
+
+    <div class="flex items-center mt-6 justify-between px-2">
+      <nav class="flex">
+        <a href="https://github.com/phvm" rel="noopener noreferrer" target="_blank">
+          <Icon class="text-3xl" icon="iconoir:github" />
+        </a>
+        <a href="https://linkedin.com/in/-pedromoura/" rel="noopener noreferrer" target="_blank">
+          <Icon class="text-3xl" icon="mdi:linkedin" />
+        </a>
+      </nav>
+      <a
+        class="border-2 p-1 rounded-md bg-accent border-foreground text-accentForeground"
+        download="cv"
+        href="public/Currículo.pdf"
+      >
+        <button>Download my CV</button>
+      </a>
+    </div>
   </main>
 </template>
 
 <script lang="ts" setup>
 import NavigationLink from "@/components/NavigationLink.vue";
-import ThemeSelector from "@/components/ThemeSelector.vue";
+import { Icon } from "@iconify/vue";
 import { navLinks } from "@/constants";
 </script>
