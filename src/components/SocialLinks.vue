81c00fdd43ecd0acb542b0a6813a4dbe82487cf7
phvm <pedrohvdm@gmail.com>
2024-12-03 17:21:42 -0300

    refactor: social media links into components

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+
+interface Props {
+  icon: string;
+  title: string;
+}
+
+const { icon, title } = defineProps<Props>();
+</script>
+
+<template>
+  <a
+    class="mr-4 flex items-end border-b-2 transition-colors duration-300 hover:border-accent"
+    href="https://github.com/phvm"
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    <Icon :icon="icon" class="text-3xl" />
+    <span>{{ title }}</span>
+  </a>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,27 @@
 <template>
-  <main class="flex flex-col mx-auto px-2 lg:p-12 lg:w-1/2">
-    <h1 class="text-xl fo font-bold text-left mb-3 md:text-4xl">Pedro Moura</h1>
+  <main class="flex flex-col mx-auto px-4 lg:p-12 lg:w-1/2">
+    <PageTitle title="Pedro Moura" />
     <h2 class="my-2 text-lg font-medium">
       Hey, I'm Pedro Moura, I'm a software developer specialized in
       <strong class="text-accent">front-end development.</strong>
     </h2>
     <p class="my-2 text-base/7">
       My favorite work is to build software that looks great, is accessible to everyone and is blazing fast,
-      meticulously crafting with accessibility, responsiveness and performance in mind. <br />
+      meticulously crafting with accessibility, responsiveness and performance in mind.
+    </p>
+    <p class="my-4 text-base/7">
       You can see some
       <NavigationLink :link="navLinks.projects.link">projects I've already built</NavigationLink>
-      (sometimes with wonderful people) or if you're curious about my past experiences as a software developer,
+      (sometimes with wonderful people) or if you're curious about my experiences history as a software developer,
       <NavigationLink :link="navLinks.experiences.link"> you can go here.</NavigationLink>
     </p>
-
-    <div class="flex items-center mt-6 justify-between px-2">
+    <span class="mt-6 mb-2">Find me or come say hi on</span>
+    <div class="flex items-center justify-between px-2">
       <nav class="flex">
-        <a class="mr-4" href="https://github.com/phvm" rel="noopener noreferrer" target="_blank">
-          <Icon class="text-3xl" icon="iconoir:github" />
-        </a>
-        <a class="mx-4" href="https://linkedin.com/in/-pedromoura/" rel="noopener noreferrer" target="_blank">
-          <Icon class="text-3xl" icon="mdi:linkedin" />
-        </a>
+        <SocialLinks icon="iconoir:github" title="Github" />
+        <SocialLinks icon="mdi:linkedin" title="LinkedIn" />
       </nav>
+
       <a
         class="border-2 p-1 rounded-md bg-accent border-foreground text-accentForeground"
         download="cv"
@@ -36,6 +35,7 @@
 
 <script lang="ts" setup>
 import NavigationLink from "@/components/NavigationLink.vue";
-import { Icon } from "@iconify/vue";
+import SocialLinks from "@/components/SocialLinks.vue";
+import PageTitle from "@/components/PageTitle.vue";
 import { navLinks } from "@/constants";
 </script>
