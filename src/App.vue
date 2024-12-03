52aabeb0e20ad4546a5ba034fc4473647107c580
Pedro Moura <Pedrohvdm@gmail.com>
2024-12-03 11:50:04 -0300

    feat: navbar with theme selector and links

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 import { RouterView } from "vue-router";
+import PageNavbar from "@/components/PageNavbar.vue";
 </script>
 
 <template>
+  <PageNavbar/>
   <RouterView />
 </template>
---
+++ src/components/PageNavbar.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import ThemeSelector from "@/components/ThemeSelector.vue";
+import NavbarLink from "@/components/NavbarLink.vue";
+import { NavbarLinks } from "@/constants";
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <nav class="flex justify-evenly px-2 py-2 mb-4 md:px-6 md:justify-end">
+    <NavbarLink
+      v-for="(navbarLink, index) in NavbarLinks"
+      :key="index"
+      :link="navbarLink.link"
+      :title="navbarLink.title"
+    >
+      <Icon :icon="navbarLink.icon" />
+    </NavbarLink>
+    <ThemeSelector />
+  </nav>
+</template>
+
+<style scoped></style>
