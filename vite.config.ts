eae64ce9e8edfffab9b4c9081b77e167ff9600e4
Pedro Moura <Pedrohvdm@gmail.com>
2024-12-14 17:29:02 -0300

    feat: https enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), basicSsl()],
+  server: { https: true },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
