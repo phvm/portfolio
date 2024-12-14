7e224c39f0cbe31983bcc1032a7244157f6bef9c
Pedro Moura <Pedrohvdm@gmail.com>
2024-12-14 18:07:56 -0300

    fix: vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), basicSsl()],
-  server: { https: {} },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
