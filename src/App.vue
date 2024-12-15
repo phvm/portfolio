80c2275fbf13d020eb4317e71878a4891cb003be
Pedro Moura <Pedrohvdm@gmail.com>
2024-12-15 08:24:22 -0300

    feat: device size based transition

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script lang="ts" setup>
 import { RouterView } from "vue-router";
 import PageNavbar from "@/components/PageNavbar.vue";
+import { useScreenSize } from "@/composables/useScreenSize";
+import { computed } from "vue";
+
+const isMobile = useScreenSize();
+const translateDistance = computed(() => {
+  return isMobile.value ? "translate-y-10" : "translate-y-16";
+});
 </script>
 
 <template>
   <PageNavbar />
   <RouterView v-slot="{ Component }" class="transition-all">
     <Transition
-      enter-active-class="ease-in duration-300"
-      enter-from-class="opacity-0"
-      leave-active-class="ease-out duration-300"
-      leave-to-class="opacity-0"
+      :enter-from-class="`opacity-0 ${translateDistance}`"
+      :leave-to-class="`opacity-0 -${translateDistance}`"
+      enter-active-class="ease-in duration-200"
+      leave-active-class="ease-in-out duration-200"
       mode="out-in"
     >
       <component :is="Component" />
--- src/composables/useScreenSize.ts
+++ src/composables/useScreenSize.ts
@@ -1,14 +1,10 @@
-import { onMounted, onUnmounted, ref, watchEffect } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
 export function useScreenSize() {
   const mobileSize = 640;
   const isMobile = ref<boolean>(window.innerWidth <= mobileSize);
 
   const calculateWidth = () => (isMobile.value = window.innerWidth < mobileSize);
-
-  watchEffect(() => {
-    console.log(isMobile.value);
-  });
 
   onMounted(() => {
     window.addEventListener("resize", calculateWidth);
