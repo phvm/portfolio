7bfc4268eccd1bf1b20b33bd341d427495113fd8
phvm <pedrohvdm@gmail.com>
2024-12-15 02:50:25 -0300

    refactor: extract screen size detection as a composable

--- src/components/NavbarLink.vue
+++ src/components/NavbarLink.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { RouterLink } from "vue-router";
-import { onMounted, onUnmounted, ref } from "vue";
 import { Icon } from "@iconify/vue";
+import { useScreenSize } from "@/composables/useScreenSize";
 
 interface Props {
   link: string;
@@ -10,17 +10,7 @@ interface Props {
   isActive: boolean;
 }
 
-const mobileSize = 640;
-const isMobile = ref<boolean>(window.innerWidth <= mobileSize);
-const calculateWidth = () => (isMobile.value = window.innerWidth < mobileSize);
-
-onMounted(() => {
-  window.addEventListener("resize", calculateWidth);
-});
-
-onUnmounted(() => {
-  window.removeEventListener("resize", calculateWidth);
-});
+const isMobile = useScreenSize();
 
 const { link, title, icon, isActive } = defineProps<Props>();
 </script>
@@ -32,7 +22,8 @@ const { link, title, icon, isActive } = defineProps<Props>();
     class="group/navlink border-b-2 mx-1 md:mx-3 md:py-1 text-foreground transition-colors hover:text-foregroundDeep"
   >
     <button class="flex items-center font-medium transition-all group-active/navlink:translate-y-1.5">
-      <Icon :icon :v-if="!isMobile" class="text-xl mx-0.5" />
+      <Icon v-if="!isMobile" :icon="icon" class="text-xl mx-0.5" />
+
       {{ title.charAt(0).toUpperCase() + title.slice(1) }}
     </button>
   </RouterLink>
